65acce8faebe88ad8557f38372777c88bb4b7b69
Ju Nogueira <jucilainenogueira@gmail.com>
2024-05-18 15:24:02 -0300

    refactor: update home copy

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <div class="space-y-24">
-      <HomeIntro />
-      <HomeSocialLinks />
-      <HomeFeaturedProjects />
-      <HomeFeaturedArticles />
-      <HomeNewsletter />
-    </div>
+  <div class="space-y-24">
+    <HomeIntro />
+    <HomeSocialLinks />
+    <HomeFeaturedProjects />
+    <HomeFeaturedArticles />
+    <HomeNewsletter />
   </div>
 </template>
